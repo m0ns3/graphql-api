30bf97acfc4beabd270b7023b6f8c01d1e55b5ee
monse <monse.diaz.e@gmail.com>
2020-05-31 10:17:54 -0300

    Query all Courses, Students and Grades

--- index.js
+++ index.js
@@ -14,10 +14,93 @@ app.listen(3000, () => {
   console.log("Server running");
 });
 
+const courses = require("./courses.json");
+const students = require("./students.json");
+const grades = require("./grades.json");
+
+const CourseType = new GraphQLObjectType({
+  name: "Course",
+  description: "Represent courses",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    description: { type: GraphQLNonNull(GraphQLString) }
+  })
+});
+
+const StudentType = new GraphQLObjectType({
+  name: "Student",
+  description: "Represent students",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    lastname: { type: GraphQLNonNull(GraphQLString) },
+    courseId: { type: GraphQLNonNull(GraphQLInt) },
+    course: {
+      type: CourseType,
+      resolve: student => {
+        return courses.find(course => course.id === student.courseId);
+      }
+    }
+  })
+});
+
+const GradeType = new GraphQLObjectType({
+  name: "Grade",
+  description: "Represent grades",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    grade: { type: GraphQLNonNull(GraphQLString) },
+    courseId: { type: GraphQLNonNull(GraphQLInt) },
+    course: {
+      type: CourseType,
+      resolve: grade => {
+        return courses.find(course => course.id === grade.courseId);
+      }
+    },
+    studentId: { type: GraphQLNonNull(GraphQLInt) },
+    student: {
+      type: StudentType,
+      resolve: grade => {
+        return students.find(student => student.id === grade.studentId);
+      }
+    }
+  })
+});
+
+//Query
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Root Query",
+  fields: () => ({
+    courses: {
+      type: new GraphQLList(CourseType),
+      description: "List of All Courses",
+      resolve: () => courses
+    },
+    students: {
+      type: new GraphQLList(StudentType),
+      description: "List of All Students",
+      resolve: () => students
+    },
+    grades: {
+      type: new GraphQLList(GradeType),
+      description: "List of All Grades",
+      resolve: () => grades
+    }
+  })
+});
+
+//Schema
+const schema = new GraphQLSchema({
+  query: RootQueryType
+  //mutation: RootMutationType
+});
+
 app.use(
   "/graphql",
   expressGraphQL({
-    //schema: schema,
+    schema: schema,
     graphiql: true
   })
 );
